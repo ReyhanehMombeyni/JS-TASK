de37277e804d3d9e3ee7881c25e066a1a16c8041
Reyhaneh <reyhaneh.sa.mo@gmail.com>
2025-01-29 01:46:16 +0330

    add BMI Function.

--- js/app.js
+++ js/app.js
@@ -43,3 +43,26 @@ function isWithin (num, min, max) {
 }
 isWithin (17, 10, 15);
 
+
+// BMI Function
+const weight=0, height=0;
+function BMI (weight, height) {
+
+    const bmi= (weight/(height^2)).toFixed(2);
+    console.log("Your BMI is:",bmi)
+
+    if (bmi<18.5) {
+        console.log("You are Underweight.")
+    }
+    else if (bmi>=18.5 && bmi<=25) {
+        console.log("You are Normalweight.")
+    }
+    else if (bmi>25 && bmi<=30) {
+        console.log("You are Overweight.")
+    }
+    else {
+        console.log("You are Obesity.")
+    }
+}
+BMI (70, 1.75)
+
